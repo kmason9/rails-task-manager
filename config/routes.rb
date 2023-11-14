9007ac0d54777e4021eaf22d56046353f9b2d120
Kirsty Mason <kirsty_mason9@hotmail.com>
2023-11-14 15:29:57 +0000

    Task List with rails

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # root to: "tasks#index"
+  # get "tasks/new", to: "tasks#new", as: :task_create
+  # post "tasks", to: "tasks#create"
+
+  # get "/tasks", to: "tasks#index"
+  # get "/tasks/:id", to: "tasks#show", as: :task
+
+  # get "/tasks/:id/edit", to: "tasks#edit", as: :task_edit
+  # patch "/tasks/:id", to: "tasks#update"
+
+  # delete "/tasks/:id", to: "tasks#destroy"
+
+  resources :tasks
 end
